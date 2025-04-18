1de4f8afad5be8d6dda470ef50a7c413f36f2e7a
dora <leo.cai@xesine.com>
2025-04-18 21:24:44 +0800

    modify oks_loss

--- yoloxe/models/losses.py
+++ yoloxe/models/losses.py
@@ -125,10 +125,10 @@ class OKSLoss(nn.Module):
         super().__init__()
         self.num_kpts = num_kpts
         kpts_weight = torch.tensor(kpts_weight) if kpts_weight is not None and len(kpts_weight)==num_kpts else torch.tensor([1]*num_kpts)
-        kpts_weight = torch.clip(kpts_weight, 0.8, 3.6)
+        kpts_weight = torch.clip(kpts_weight, 0.5, 2.0)
         self.sigmas = torch.tensor([1/num_kpts]*num_kpts) / kpts_weight
         self.reduction = reduction
-        self.dist_loss = nn.SmoothL1Loss(reduction="none")
+        self.dist_loss = nn.MSELoss(reduction="none")
         self.conf_loss = FocalLoss(reduction="none")
 
     def forward(self, kpts_pred, kpts_conf_pred, \
@@ -139,12 +139,10 @@ class OKSLoss(nn.Module):
         if kpts_target.shape[0]==0:
             loss_kpts = torch.ones([1, kpts_target.shape[1]], device=kpts_target.device)
         else:
-            dist_x = self.dist_loss(kpts_pred[:, 0::2], kpts_target[:, 0::2])
-            dist_y = self.dist_loss(kpts_pred[:, 1::2], kpts_target[:, 1::2])
-            dist = dist_x + dist_y
+            dist = self.dist_loss(kpts_pred[:, 0::2], kpts_target[:, 0::2]) + self.dist_loss(kpts_pred[:, 1::2], kpts_target[:, 1::2])
             bbox_area = torch.prod(bbox_targets[:, -2:], dim=1, keepdim=True)  # scale derived from bbox gt: w*h
             kpts_loss_factor = (torch.sum(kpts_conf_target != 0) + torch.sum(kpts_conf_target == 0)) / torch.clip(torch.sum(kpts_conf_target != 0), 1e-9)
-            oks = torch.exp(-dist / torch.clip((bbox_area**0.5) * (sigmas**2), 1e-9))
+            oks = torch.exp(-dist / torch.clip(bbox_area * (4*sigmas**2), 1e-9))
             loss_kpts = kpts_loss_factor * ((1 - oks) * kpts_conf_target)
         loss_kpts = loss_kpts.mean(axis=1)
 
